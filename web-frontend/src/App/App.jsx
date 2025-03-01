32996cb078a8046b5f364a8e973d0419d67485f6
yuxru <yuxiaje@gmail.com>
2025-02-28 22:29:01 -0800

    Add files via upload
    from react-redux-vite-scaffold

---
+++ web-frontend/src/App/App.jsx
@@ -0,0 +1,7 @@
+import styles from "./App.module.css";
+
+const App = () => {
+    return <div className={styles.container}>hello werld</div>;
+};
+
+export default App;
---
+++ web-frontend/src/mainSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const mainSlice = createSlice({
+    name: "main",
+    initialState: {},
+    reducers: {},
+});
+
+export default mainSlice;
